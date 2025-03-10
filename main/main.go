73eb29c69f7712c6580de76a34404581967fc5fe
Matheus Oliveira da Silva <matheus.6148@gmail.com>
2025-03-10 07:20:24 -0300

    created a insert and delete methods

--- main/main.go
+++ main/main.go
@@ -45,6 +45,29 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Printf("Albums found: %v\n", albums)
+
+	alb, err := albumByID(2)
+	if err != nil {
+    log.Fatal(err)
+	}
+	fmt.Printf("Album found: %v\n", alb)
+
+	albID, err := addAlbum(Album{
+		Title: "The Modern Sounf of Betty Carter",
+		Artist: "Betty Cartr",
+		Price: 49.95,
+	})
+	if err !=nil{
+		log.Fatal(err)
+	}
+	fmt.Printf("ID of added album: %d\n", albID)
+
+	albumRemoved, err := removeByID(8)
+	if err != nil {
+    log.Fatal(err)
+	}
+	fmt.Printf("Album ID removed: %v\n", albumRemoved)
+
 }
 
 func albumsByArtist(name string) ([]Album, error){
@@ -67,4 +90,45 @@ func albumsByArtist(name string) ([]Album, error){
 		return nil, fmt.Errorf("albumsByArtist: %q: %v", name, err)
 	}
 	return albums, nil
+}
+
+func albumByID(id int64) (Album, error){
+	var alb Album
+
+	row := db.QueryRow("SELECT * FROM album WHERE id = ?", id)
+	if err := row.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil{
+		if err == sql.ErrNoRows {
+			return alb, fmt.Errorf("albumByID: %d: no such album", id)
+		}
+		return alb, fmt.Errorf("albumByID: %d: %v", id, err)
+	}
+	return alb, nil
+}
+
+func addAlbum(a Album) (int64, error){
+	result, err := db.Exec("INSERT INTO album (title, artist, price) VALUES (?,?,?)", a.Title, a.Artist, a.Price)
+	if err !=nil {
+		return 0, fmt.Errorf("addAlbum: %v", err)
+	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("addAlbum: %v", err)
+	}
+	return id, nil
+}
+
+func removeByID(id int64) (int64, error){
+	result, err := db.Exec("DELETE FROM album WHERE id = ?", id)
+	if err != nil {
+		return 0, fmt.Errorf("removeByID: %v", err)
+	}
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return 0, fmt.Errorf("removeByID: %v", err)
+	}
+	if rowsAffected == 0 {
+        return 0, fmt.Errorf("removeByID: no album found with id %d", id)
+    }
+
+	return id, nil
 }
